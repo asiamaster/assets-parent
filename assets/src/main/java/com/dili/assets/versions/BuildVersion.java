13197d408ae82a7851ffb4358d35e66631fefc4c
wangguofeng <wangguofeng@diligrp.com>
2020-01-19 15:17:05 +0800

    添加注释

--- assets/src/main/java/com/dili/assets/versions/BuildVersion.java
+++ assets/src/main/java/com/dili/assets/versions/BuildVersion.java
@@ -1,7 +1,14 @@
 package com.dili.assets.versions;
-
+/**
+ * 
+ * @author admin
+ *
+ */
 public interface BuildVersion {
-
+/**
+ * 返回当前编译版本号(时间戳)
+ * @return
+ */
 	public String getVersion();
 
 }
