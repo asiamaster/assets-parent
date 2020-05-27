27bf889834854c23b95e980dda5a642967844856
岳洪波 <yuehongbo@diligrp.com>
2020-05-27 17:58:46 +0800

    增加收费项管理功能

---
+++ assets-sdk/src/main/java/com/dili/assets/sdk/dto/ChargeItemDto.java
@@ -0,0 +1,130 @@
+package com.dili.assets.sdk.dto;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.dili.ss.domain.BaseDomain;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+/**
+ * <B></B>
+ * <B>Copyright:本软件源代码版权归农丰时代科技有限公司及其研发团队所有,未经许可不得任意复制与传播.</B>
+ * <B>农丰时代科技有限公司</B>
+ *
+ * @author yuehongbo
+ * @date 2020/5/26 18:31
+ */
+public class ChargeItemDto extends BaseDomain {
+
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 收费项名称
+     */
+    private String name;
+
+    /**
+     * 是否启用
+     * @see com.dili.commons.glossary.YesOrNoEnum
+     */
+    private Integer isEnable;
+
+    /**
+     * 是否删除
+     * {@link com.dili.commons.glossary.YesOrNoEnum}
+     */
+    private Integer isDelete;
+
+    /**
+     * 备注
+     */
+    private String notes;
+
+    /**
+     * 创建人
+     */
+    private Long operatorId;
+
+    /**
+     * 创建时间
+     */
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime modifyTime;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getIsEnable() {
+        return isEnable;
+    }
+
+    public void setIsEnable(Integer isEnable) {
+        this.isEnable = isEnable;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Long getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Long operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(LocalDateTime modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
