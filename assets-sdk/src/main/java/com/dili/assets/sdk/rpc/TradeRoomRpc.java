6c4b7f8bb66396a7ff4fc9e85a186a16b44ec597
shaofan <akhuting@hotmail.com>
2020-08-06 16:15:42 +0800

    * 新增交易类型和交易厅rpc

--- assets-sdk/src/main/java/com/dili/assets/sdk/rpc/TradeRoomRpc.java
+++ assets-sdk/src/main/java/com/dili/assets/sdk/rpc/TradeRoomRpc.java
@@ -1,18 +1,18 @@
 /*
-*  Copyright 2019-2020 ShaoFan
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*/
+ *  Copyright 2019-2020 ShaoFan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.dili.assets.sdk.rpc;
 
 import com.dili.assets.sdk.dto.TradeRoomDto;
@@ -24,40 +24,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
-* @website http://shaofan.org
-* @author shaofan
-* @date 2020-07-30
-**/
-@FeignClient(name = "tradeRoom-service", url = "http://127.0.0.1:8182")
+ * @author shaofan
+ * @website http://shaofan.org
+ * @date 2020-07-30
+ **/
+@FeignClient(name = "tradeRoom-service", url = "${AssetsRpc.url:}")
 public interface TradeRoomRpc {
 
     /**
-    * 查询交易厅数据分页
-    */
+     * 查询交易厅数据分页
+     */
     @RequestMapping(value = "/api/tradeRoom/query", method = RequestMethod.POST)
     TableResult<TradeRoomDto> query(TradeRoomQuery query);
 
     /**
-    * 新增交易厅
-    */
+     * 新增交易厅
+     */
     @RequestMapping(value = "/api/tradeRoom/add", method = RequestMethod.POST)
     BaseOutput add(TradeRoomDto dto);
 
     /**
-    * 修改交易厅
-    */
+     * 修改交易厅
+     */
     @RequestMapping(value = "/api/tradeRoom/update", method = RequestMethod.POST)
     BaseOutput update(TradeRoomDto dto);
 
     /**
-    * 多选删除交易厅
-    */
+     * 多选删除交易厅
+     */
     @RequestMapping(value = "/api/tradeRoom/deleteAll", method = RequestMethod.POST)
     BaseOutput deleteAll(Long[] ids);
 
     /**
-    * 获取交易厅
-    */
+     * 获取交易厅
+     */
     @RequestMapping(value = "/api/tradeRoom/get", method = RequestMethod.POST)
     BaseOutput<TradeRoomDto> get(Long id);
 
