48c31ec3efb8582f34e818f656cc1d806355ec5f
shaofan <akhuting@hotmail.com>
2020-10-16 14:28:53 +0800

    * 修改销地不能重复

--- assets/src/main/java/com/dili/assets/service/impl/PosMarketServiceImpl.java
+++ assets/src/main/java/com/dili/assets/service/impl/PosMarketServiceImpl.java
@@ -1,22 +1,25 @@
 /*
-*  Copyright 2019-2020 ShaoFan
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*/
+ *  Copyright 2019-2020 ShaoFan
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.dili.assets.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import com.dili.assets.domain.PosMarket;
+import com.dili.assets.sdk.dto.PosMarketDto;
 import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.dto.IDTO;
 import lombok.RequiredArgsConstructor;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -25,14 +28,15 @@ import com.dili.assets.sdk.dto.PosMarketQuery;
 import com.dili.assets.mapper.PosMarketMapper;
 import org.springframework.stereotype.Service;
 import com.dili.ss.base.BaseServiceImpl;
+
 import java.util.List;
 
 /**
-* @website http://shaofan.org
-* @description 服务实现
-* @author shaofan
-* @date 2020-10-15
-**/
+ * @author shaofan
+ * @website http://shaofan.org
+ * @description 服务实现
+ * @date 2020-10-15
+ **/
 @Service
 @RequiredArgsConstructor
 public class PosMarketServiceImpl extends BaseServiceImpl<PosMarket, Long> implements PosMarketService {
@@ -40,7 +44,7 @@ public class PosMarketServiceImpl extends BaseServiceImpl<PosMarket, Long> imple
     private final PosMarketMapper posMarketMapper;
 
     @Override
-    public PageInfo<PosMarket> queryAll(PosMarketQuery query){
+    public PageInfo<PosMarket> queryAll(PosMarketQuery query) {
         PageHelper.startPage(query.getPageNum(), query.getPageSize());
         List<PosMarket> list = posMarketMapper.selectByQuery(query);
         PageInfo<PosMarket> pageInfo = new PageInfo<>(list);
@@ -70,12 +74,40 @@ public class PosMarketServiceImpl extends BaseServiceImpl<PosMarket, Long> imple
 
     @Override
     public BaseOutput savePosMarket(PosMarket posMarket) {
+        PosMarket query = new PosMarket();
+        query.setName(posMarket.getName());
+        query.setMarket(posMarket.getMarket());
+        if (CollUtil.isNotEmpty(this.listByExample(query))) {
+            return BaseOutput.failure("名称重复");
+        }
+
+        query = new PosMarket();
+        query.setArea(posMarket.getArea());
+        query.setMarket(posMarket.getMarket());
+        if (CollUtil.isNotEmpty(this.listByExample(query))) {
+            return BaseOutput.failure("区域重复");
+        }
         this.saveOrUpdate(posMarket);
         return BaseOutput.success();
     }
 
     @Override
     public BaseOutput updatePosMarket(PosMarket posMarket) {
+        PosMarket query = new PosMarket();
+        query.setName(posMarket.getName());
+        query.setMarket(posMarket.getMarket());
+        query.setMetadata(IDTO.AND_CONDITION_EXPR, "id !=" + posMarket.getId());
+        if (CollUtil.isNotEmpty(this.listByExample(query))) {
+            return BaseOutput.failure("名称重复");
+        }
+
+        query = new PosMarket();
+        query.setArea(posMarket.getArea());
+        query.setMarket(posMarket.getMarket());
+        query.setMetadata(IDTO.AND_CONDITION_EXPR, "id !=" + posMarket.getId());
+        if (CollUtil.isNotEmpty(this.listByExample(query))) {
+            return BaseOutput.failure("区域重复");
+        }
         this.updateSelective(posMarket);
         return BaseOutput.success();
     }
