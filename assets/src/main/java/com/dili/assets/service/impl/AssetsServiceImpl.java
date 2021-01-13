cc61cefa47d2e1ad89990d5239f329f307f99a71
shaofan <akhuting@hotmail.com>
2021-01-13 14:14:53 +0800

    修复bug

--- assets/src/main/java/com/dili/assets/service/impl/AssetsServiceImpl.java
+++ assets/src/main/java/com/dili/assets/service/impl/AssetsServiceImpl.java
@@ -231,7 +231,7 @@ public class AssetsServiceImpl extends BaseServiceImpl<Assets, Long> implements 
         query.setRows(100);
 
         var list = listPageByExample(query).getDatas();
-        if (query.isOnlyFirstArea() && query.getSecondArea() == null) {
+        if (query.isOnlyFirstArea() && query.getArea() != null && query.getSecondArea() == null) {
             list = list.stream().filter(it -> it.getSecondArea() == null).collect(Collectors.toList());
         }
 
